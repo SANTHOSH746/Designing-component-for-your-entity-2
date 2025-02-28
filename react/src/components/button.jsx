95ada3ee90a7b2537f32e193f71d314188ada55a
SANTHOSH746 <santhoshrca2006@gmail.com>
2025-02-28 09:56:58 +0530

    fcc

---
+++ react/src/components/button.jsx
@@ -0,0 +1,13 @@
+// write button card here
+// src/components/button.jsx
+import React from "react";
+
+const ViewProductButton = () => {
+  return (
+    <button className="bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-600 transition">
+      View Product
+    </button>
+  );
+};
+
+export default ViewProductButton;
